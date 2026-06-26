4f41348a3e47027efadcb9ae3c4e6c1eff36fa88
V12112 <4toaesc18@gmail.com>
2026-06-26 14:01:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
@@ -22,6 +23,8 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000, () => {
-    console.log("Servidor iniciado en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
